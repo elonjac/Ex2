9da2e3468886de4654e9a8fcc05a56f9f23d6178
elonj <elonjacobowitz@gmail.com>
2025-01-06 11:41:12 +0200

    scell good need to finish ex2sheet

--- src/ex2/CellEntry.java
+++ src/ex2/CellEntry.java
@@ -2,15 +2,62 @@ package ex2;
 // Add your documentation below:
 
 public class CellEntry  implements Index2D {
+    private int x;
+    private int y;
 
-    @Override
-    public boolean isValid() {
+    public CellEntry(String index) {
+        if(index == null || index.isEmpty()) {
+            this.x = Ex2Utils.ERR;
+            this.y = Ex2Utils.ERR;
+        }
+        else{
+            parseIndex(index);
+        }
+    }
+    private void parseIndex(String index) {
+        int col = 0;
+        int i = 0;
+        while (i < index.length() &&Character.isLetter(index.charAt(i))) {
+            col = col * 26 + (Character.toUpperCase(index.charAt(i)) - 'A' +1);
+        }
+        int row = Integer.parseInt(index.substring(i));
+        this.x = col;
+        this.y = row;
+    }
+    private boolean isValidIndex(String index) {
+        if(index != null && index.matches("(?i)[A-Z]+\\d{1,2}")) {
+            return true;
+        }
         return false;
     }
 
     @Override
-    public int getX() {return Ex2Utils.ERR;}
+    public boolean isValid() {
+        if(this.x != Ex2Utils.ERR && this.y != Ex2Utils.ERR){
+            return true;
+        }
+        return false;
+    }
 
     @Override
-    public int getY() {return Ex2Utils.ERR;}
+    public int getX() {
+        return this.x;
+    }
+
+    @Override
+    public int getY() {
+        return this.y;
+    }
+    public String toString() {
+        if(!isValid()){
+            return "Invalid";
+        }
+        StringBuilder sb = new StringBuilder();
+        int temp = x;
+        while(temp > 0){
+            sb.insert(0,(char) ('A' + (temp % 26)));
+            temp = temp / 26;
+        }
+        return sb + Integer.toString(y);
+    }
 }
--- src/ex2/Ex2Sheet.java
+++ src/ex2/Ex2Sheet.java
@@ -1,8 +1,5 @@
 package ex2;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 // Add your documentation below:
 
 public class Ex2Sheet implements Sheet {
@@ -14,7 +11,7 @@ public class Ex2Sheet implements Sheet {
         table = new SCell[x][y];
         for (int i = 0; i < x; i = i + 1) {
             for (int j = 0; j < y; j = j + 1) {
-                table[i][j] = new SCell("");
+                table[i][j] = new SCell(Ex2Utils.EMPTY_CELL);
             }
         }
         eval();
@@ -48,19 +45,12 @@ public class Ex2Sheet implements Sheet {
     public Cell get(String cords) {
         Cell ans = null;
         // Add your code here
-        if (cords == null || cords.isEmpty()) {
-            return ans;
+        CellEntry cellEntry = new CellEntry(cords);
+        if(cellEntry.isValid()) {
+            int x = cellEntry.getX();
+            int y = cellEntry.getY();
+            ans = get(x, y);
         }
-        char column = cords.charAt(0);
-        String row = cords.substring(1);
-        int colindex = column - 'A';
-        int  rowindex = Integer.parseInt(row) ;
-
-        if (!isIn(rowindex, colindex)) {
-            return ans;
-        }
-        ans = get(colindex, rowindex);
-
         /////////////////////
         return ans;
     }
@@ -82,10 +72,7 @@ public class Ex2Sheet implements Sheet {
         if(isIn(x, y)) {
             Cell c = new SCell(s);
             table[x][y] = c;
-
         }
-
-
         /////////////////////
     }
 
@@ -93,6 +80,11 @@ public class Ex2Sheet implements Sheet {
     public void eval() {
         int[][] dd = depth();
         // Add your code here
+        for(int i = 0; i < width(); i++) {
+            for(int j = 0; j < height(); j++) {
+                eval(i,j);
+            }
+        }
 
 
 
@@ -114,38 +106,6 @@ public class Ex2Sheet implements Sheet {
     public int[][] depth() {
         int[][] ans = new int[width()][height()];
         // Add your code here
-        for(int i = 0; i < width(); i++) {
-            for(int j = 0; j < height(); j++) {
-                Cell cell = get(i, j);
-                String data = cell.getData().trim();
-                if(data.startsWith("=")){
-                    String formula = data.substring(1);
-                    String[] parts = formula.split("[+\\-*/()]");
-                    int maxDepth = 0;
-
-                    for(String part : parts) {
-                        part = part.trim();
-                        if(part.matches("[A-Z]+[0-9]+")) {
-                            int col = part.charAt(0) - 'A';
-                            int row = Integer.parseInt(part.substring(1));
-
-                            if(isIn(row, col)) {
-                                maxDepth= Math.max(maxDepth, ans[row][col]);
-                            }
-                        }
-                    }
-                    ans[i][j] = maxDepth + 1;
-                    cell.setOrder(ans[i][j]);
-                }
-                else{
-                    ans[i][j] = 0;
-                    cell.setOrder(ans[i][j]);
-                }
-            }
-        }
-
-
-
 
         // ///////////////////
         return ans;
@@ -154,6 +114,7 @@ public class Ex2Sheet implements Sheet {
     @Override
     public void load(String fileName) throws IOException {
         // Add your code here
+
 
         /////////////////////
     }
@@ -168,41 +129,22 @@ public class Ex2Sheet implements Sheet {
     @Override
     public String eval(int x, int y) {
         String ans = null;
-        if(get(x,y)!=null) {
+        if (get(x, y) != null) {
             String data = get(x, y).getData();
-
-            // Add your code here
-            if (ans.startsWith("=")) {
-                try {
-                    String formula = data.substring(1);
-                    for (int i = 0; i < formula.length(); i++) {
-                        if (Character.isLetter(formula.charAt(i))) {
-                            int end = i + 1;
-                            while (end < formula.length() || Character.isLetter(formula.charAt(end))) {
-                                end++;
-                            }
-                            if (end > i + 1) {
-                                String cellRef = formula.substring(i, end);
-                                Cell referenced = get(cellRef);
-                                if (referenced != null) {
-                                    String value = referenced.getData();
-                                    formula = formula.substring(0, i) + value + formula.substring(end);
-                                }
-                            }
-                        }
-                    }
-                    double result = SCell.calc(ans);
-                    ans = String.valueOf(result);
-                }catch (NumberFormatException e) {
-                    ans = Ex2Utils.ERR_FORM;
+        // Add your code here
+            if(data != null) {
+                if(SCell.isForm(data)) {
+                    ans = String.valueOf(data);
                 }
-
-            }else {
-                ans = data;
+                else {
+                    ans = data;
+                }
             }
         }
 
+
         /////////////////////
         return ans;
-        }
+    }
+
 }
